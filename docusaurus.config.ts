93ac43e5b25cd2049e95d8f7a9ca940471a1a320
Manoj L <manojLondhe@users.noreply.github.com>
2024-12-13 16:17:35 +0530

    React Projects Styleguide - ESLint, Prettier configs (#27)
    * Task #221910 : React JS Coding Standards Conventions
    
    * Cleanup react style guide, update docs for ESLint and prettier
    
    ---------
    
    Co-authored-by: Rushi G <rushi_g@tekditechnologies.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -28,8 +28,8 @@ const config: Config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: "en-GB",
+    locales: ["en-GB"],
   },
 
   presets: [
@@ -43,6 +43,9 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+
+          showLastUpdateTime: true, // ++ Tekdi
+          showLastUpdateAuthor: true, // ++ Tekdi
         },
         /*blog: {
           showReadingTime: true,
